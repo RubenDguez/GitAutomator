4752e563487d350827594ee8af562e3cb5f4b82b
Argenis Dominguez <argenisdominguez@Argeniss-MacBook-Pro.local>
2021-01-25 13:44:10 -0500

    "1611600250577"

--- src/App.java
+++ src/App.java
@@ -6,12 +6,15 @@ public class App {
         String remote = "origin";
         String branch = "main";
         String gitRepo = "https://github.com/RubenDguez/GitAutomator.git";
-        // String fileName = "src/Git.java";
-        // String[] arrFileName = { "src/Git.java", "src/App.java", "src/RunGit.java" };
+        String fileName = "src/Git.java";
+        String[] arrFileName = { "src/Git.java", "src/App.java", "src/RunGit.java" };
 
         RunGit rg = new RunGit();
-        rg.run(remote, branch, gitRepo);
-        // rg.run(fileName, remote, branch, gitRepo);
-        // rg.run(arrFileName, remote, branch, gitRepo);
+
+        rg.run(remote, branch, gitRepo); // for pushing the entire project to the repository
+        // rg.run(fileName, remote, branch, gitRepo); // for pushing an specific file to
+        // the repository
+        // rg.run(arrFileName, remote, branch, gitRepo); // for pushing and array of
+        // files to the repository
     }
 }
